cea4f208d6b01a3e54be968332264d760faa2e0d
Matthew Ascone <asconem@gmail.com>
2020-06-26 17:06:06 -0400

    Add SmokeTest testing class

--- src/test/java/com/example/testingweb/TestingWebApplicationTests.java
+++ src/test/java/com/example/testingweb/TestingWebApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.testingweb;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class TestingWebApplicationTests {
+public class TestingWebApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
